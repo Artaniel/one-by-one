d166e1cf89fa91e6fbc9aef69c6cb2a1308088e5
Artaniel <59449523+Artaniel@users.noreply.github.com>
2020-07-26 12:57:29 +0300

    Acid sprayer script
    added scripts for acid sprayer and acid drop
    added AcidTesting scene for testing Acid Sprayer
    PoligonColider moved to boss scripts folder

---
+++ Assets/Scripts/Enemy/BossEncounters/AcidDrop.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AcidDrop : MonoBehaviour
+{
+    public Vector3 targetPosition;
+    public float velocity;
+    private bool isMoving = false;
+
+    private void Update()
+    {
+        if (isMoving && !Pause.Paused) {
+            if (Vector3.Distance(transform.position, targetPosition) > velocity * Time.deltaTime)            
+                transform.position += (targetPosition - transform.position).normalized * velocity * Time.deltaTime;
+            else {
+                transform.position = targetPosition;
+                isMoving = false;
+            }
+        }
+    }
+
+    public void StartMove() {
+        isMoving = true;
+    }
+
+}
