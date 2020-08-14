66aa6a42dc2916eb201f93daff3509f90126e49c
Artaniel <59449523+Artaniel@users.noreply.github.com>
2020-08-15 00:21:28 +0300

    fire bug

---
+++ Assets/Scripts/Enemy/Enemy Behaviors/FireBug.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireBug : MonoBehaviour
+{
+    private MonsterLife monsterLife;
+    public GameObject firePrefab;
+
+    private void Awake()
+    {
+        monsterLife = GetComponent<MonsterLife>();
+    }
+
+    private void Update()
+    {
+        if (monsterLife.HP <= 0) {
+            FireOnTilemap.StartFire(transform.position, firePrefab);
+        }
+    }
+}
