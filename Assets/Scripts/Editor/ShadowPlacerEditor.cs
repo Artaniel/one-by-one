3a744d3bee8394db199ad5e9ec0e13e4830ffffa
Sadpotato <alexprgrigoriev@gmail.com>
2020-10-05 19:57:22 +0400

    Shadow for trees and collision in cell room fix

---
+++ Assets/Scripts/Editor/ShadowPlacerEditor.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(ShadowPlacer))]
+public class ShadowPlacerEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        ShadowPlacer myTarget = (ShadowPlacer)target;
+        if (GUILayout.Button("Place Shadow"))
+        {
+            myTarget.PlaceShadows();
+            EditorUtility.SetDirty(myTarget);
+        }
+    }
+}
