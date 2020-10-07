4d05f9c1027191d4d5fe79c7bafad5f4291edc56
Artaniel <59449523+Artaniel@users.noreply.github.com>
2020-10-07 21:33:25 +0300

    Laser tower

---
+++ Assets/Scripts/Enemy/Enemy Behaviors/RotatingLaserTurret.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatingLaserTurret : MonoBehaviour
+{
+    [SerializeField] private float rotationSpeed = 90f; //degree in sec
+    [SerializeField] private EnemyLaser[] lasers;
+
+    private void Awake()
+    {
+        foreach (EnemyLaser laser in lasers)
+        {
+            if (laser == null)
+                Debug.Log("LaserTurret cant find laser");
+            else
+                laser.ShootStartDirection(laser.transform.position, transform.up);
+        }
+    }
+
+    private void Start()
+    {
+        foreach (EnemyLaser laser in lasers)        
+            laser.line.enabled = true;        
+    }
+
+    private void Update()
+    {        
+        if (!Pause.Paused) {
+            transform.Rotate(Vector3.forward, rotationSpeed * Time.deltaTime);
+            foreach (EnemyLaser laser in lasers)            
+                laser.UpdateLaser(laser.transform.position, transform.up);
+        }
+    }
+}
