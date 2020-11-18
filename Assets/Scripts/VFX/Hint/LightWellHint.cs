e24750c12891894a744a5145b1bdcd37096af89b
Sadpotato <alexprgrigoriev@gmail.com>
2020-11-18 13:10:44 +0400

    Lightwell hint

---
+++ Assets/Scripts/VFX/Hint/LightWellHint.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.LWRP;
+
+public class LightWellHint : CurrentEnemyHint
+{
+    private SpriteRenderer spriteRenderer;
+    private Color startColor;
+    private Color transparentColor;
+
+    private Light2D light2D;
+    private float startIntensity;
+
+    protected override void Start()
+    {
+        spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+        startColor = spriteRenderer.color;
+        transparentColor = startColor;
+        transparentColor.a = 0;
+
+        light2D = GetComponentInChildren<Light2D>();
+        startIntensity = light2D.intensity;
+    }
+
+    protected override void SetupHintVisual(Transform parent)
+    {
+        this.parent = parent;
+    }
+
+    protected override void UpdateVisual(float timeFraction)
+    {
+        if (parent)
+        {
+            transform.position = parent.position;
+            spriteRenderer.color = Color.Lerp(transparentColor, startColor, timeFraction);
+            light2D.intensity = Mathf.Lerp(0, startIntensity, timeFraction);
+        }
+        else
+        {
+            spriteRenderer.color = Color.Lerp(startColor, transparentColor, timeFraction);
+            light2D.intensity = Mathf.Lerp(startIntensity, 0, timeFraction);
+        }
+    }
+}
---
+++ Assets/Scripts/VFX/Hint/ParticleEnemyHint.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleEnemyHint : CurrentEnemyHint
+{
+    protected override void SetupHintVisual(Transform parent)
+    {
+        particleSystem.Stop();
+        this.parent = parent;
+    }
+
+    protected override void UpdateVisual(float timeFraction)
+    {
+        if (parent)
+        {
+            transform.position = parent.position;
+            if (timeFraction >= 1 && !particleSystem.isPlaying)
+            {
+                particleSystem.Play();
+            }
+        }
+    }
+}
