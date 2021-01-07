4877a3a1d62e5afb289a4d5d5436c4c13674582e
Sadpotato <alexprgrigoriev@gmail.com>
2021-01-08 00:23:30 +0400

    New VFX in test scene

---
+++ Assets/Scripts/VFX/ActivateInCloseProximity.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateInCloseProximity : MonoBehaviour
+{
+    private Transform player;
+    public GameObject[] toActivate;
+    public float distanceToActivate = 5f;
+    private bool activated = false;
+
+    void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;    
+    }
+
+    void Update()
+    {
+        if (!activated && Vector2.Distance(player.position, transform.position) < distanceToActivate)
+        {
+            activated = true;
+            foreach (var obj in toActivate)
+            {
+                obj.SetActive(true);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/VFX/BatOneWay.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BatOneWay : MonoBehaviour
+{
+    public Vector2 speedRange = new Vector2(9f, 14f);
+    public float speed;
+    public Vector2 rotateSpeedRange = new Vector2(15f, 45f);
+    private float rotateSpeed;
+    public float rotateSign = 1f;
+    public float timeToSignChange = 0.75f;
+    private float timeToSignChangeLeft;
+
+    public int startLayer = 3;
+    public int activatedLayer = 6;
+    public float timeToLayerSwitch = 0.5f;
+    private float timeToLayerSwitchLeft;
+
+    private SpriteRenderer spriteRenderer;
+
+    void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void OnEnable()
+    {
+        rotateSpeed = Random.Range(rotateSpeedRange.x, rotateSpeedRange.y);
+        speed = Random.Range(speedRange.x, speedRange.y);
+
+        timeToLayerSwitchLeft = timeToLayerSwitch;
+        spriteRenderer.sortingOrder = startLayer;
+
+        timeToSignChangeLeft = timeToSignChange;
+    }
+    
+    void Update()
+    {
+        transform.Translate(transform.up * speed * Time.deltaTime, Space.World);
+        transform.Rotate(0, 0, rotateSign * rotateSpeed * Time.deltaTime);
+        timeToLayerSwitchLeft -= Time.deltaTime;
+        if (timeToLayerSwitchLeft < 0)
+        {
+            timeToLayerSwitchLeft = 999f;
+            spriteRenderer.sortingOrder = activatedLayer;
+        }
+        timeToSignChangeLeft -= Time.deltaTime;
+        if (timeToSignChangeLeft < 0)
+        {
+            timeToSignChangeLeft = timeToSignChange;
+            rotateSign *= -1;
+        }
+    }
+}
